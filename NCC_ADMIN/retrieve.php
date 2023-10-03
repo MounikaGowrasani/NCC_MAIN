189b99d4f086acb89b4d4b47bee6b8d317671111
Hemasri Yakkati <hemasri.yakkati1816@gmail.com>
2023-10-03 08:50:18 +0530

    hema

--- NCC_ADMIN/retrieve.php
+++ NCC_ADMIN/retrieve.php
@@ -45,81 +45,97 @@
     </style>
 </head>
 <body>
-    <?php
-    if (isset($_POST["update"])) {
-        // Database connection parameters
-        $servername = "localhost";
-        $username = "root";
-        $password = "";
-        $dbname = "ncc";
+<?php
+if (isset($_POST["update"])) {
+    // Database connection parameters
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+    $dbname = "ncc";
 
-        // Create a database connection
-        $conn = new mysqli($servername, $username, $password, $dbname);
+    // Create a database connection
+    $conn = new mysqli($servername, $username, $password, $dbname);
 
-        // Check connection
-        if ($conn->connect_error) {
-            die("Connection failed: " . $conn->connect_error);
+    // Check connection
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
+
+    // Function to safely close the database connection
+    function closeConnection($conn) {
+        if ($conn) {
+            $conn->close();
         }
+    }
 
+    try {
         $scheduleType = $_POST['schedule_type'];
-
-        // Get the current year
         $currentYear = date('Y');
 
-        // Check if a file was uploaded successfully
         if (isset($_FILES['schedule_file'])) {
-            // File details
             $fileName = $_FILES['schedule_file']['name'];
             $fileTmpName = $_FILES['schedule_file']['tmp_name'];
 
-            // Read the file content
-            $fileContent = file_get_contents($fileTmpName);
+            // Ensure it's a PDF file
+            if (pathinfo($fileName, PATHINFO_EXTENSION) === 'pdf') {
+                $fileContent = file_get_contents($fileTmpName);
+                $escapedFileName = $conn->real_escape_string($fileName);
+                $escapedFileContent = $conn->real_escape_string($fileContent);
 
-            // Escape and insert the filename and file content into the database
-            $escapedFileName = $conn->real_escape_string($fileName);
-            $escapedFileContent = $conn->real_escape_string($fileContent);
+                $unit = ($scheduleType === 'update1') ? '10A' : '25A';
 
-            // Determine the unit based on $scheduleType (update1 or update2)
-            $unit = ($scheduleType === 'update1') ? '10A' : '25A';
+                // Check if a record with the given criteria exists in the database
+                $sql_check = "SELECT * FROM pdf_files WHERE unit = '$unit' AND years = '$currentYear'";
+                $result = $conn->query($sql_check);
 
-            // SQL query for updating the database
-            $sql = "UPDATE pdf_files SET file_name = '$escapedFileName', file_content = '$escapedFileContent' 
-                    WHERE unit = '$unit' AND years = '$currentYear'";
+                if ($result && $result->num_rows > 0) {
+                    // If the record exists, update it
+                    $sql = "UPDATE pdf_files SET file_name = '$escapedFileName', file_content = '$escapedFileContent' 
+                            WHERE unit = '$unit' AND years = '$currentYear'";
+                } else {
+                    // If the record doesn't exist, insert a new record
+                    $sql = "INSERT INTO pdf_files (unit, years, file_name, file_content)
+                            VALUES ('$unit', '$currentYear', '$escapedFileName', '$escapedFileContent')";
+                }
 
-            if ($conn->query($sql) === TRUE) {
-                echo "File updated successfully.";
+                if ($conn->query($sql) === TRUE) {
+                    echo "File updated successfully.";
+                } else {
+                    echo "Error updating file: " . $conn->error;
+                }
             } else {
-                echo "Error updating file: " . $conn->error;
+                echo "Error: File must be a PDF.";
             }
         } else {
             echo "Error: File upload failed or no file selected.";
         }
-
-        // Close the database connection
-        $conn->close();
+    } catch (Exception $e) {
+        echo "Error: " . $e->getMessage();
+    } finally {
+        closeConnection($conn);
     }
-    ?>
-
-    <h2>Retrieve PDF</h2>
-    <div class="container">
-        <a href="retrieve1.php" target="_self">View ANO1(10A) Schedule</a>
-        <br>
-        <form action="" method="post" enctype="multipart/form-data">
-            <input type="file" name="schedule_file" accept=".pdf">
-            <input type="hidden" name="schedule_type" value="update1">
-            <button type="submit" name="update" id="update">Update Schedule</button>
-        </form>
-    </div>
+}
+?>
+<h2>Retrieve PDF</h2>
+<div class="container">
+    <a href="retrieve1.php" target="_self">View ANO1(10A) Schedule</a>
     <br>
+    <form action="" method="post" enctype="multipart/form-data">
+        <input type="file" name="schedule_file" accept=".pdf">
+        <input type="hidden" name="schedule_type" value="update1">
+        <button type="submit" name="update" id="update">Update Schedule</button>
+    </form>
+</div>
+<br>
+<br>
+<div class="container">
+    <a href="retrieve2.php">View ANO2(25A) Schedule</a>
     <br>
-    <div class="container">
-        <a href="retrieve2.php">View ANO2(25A) Schedule</a>
-        <br>
-        <form action="" method="post" enctype="multipart/form-data">
-            <input type="file" name="schedule_file" accept=".pdf">
-            <input type="hidden" name="schedule_type" value="update2">
-            <button type="submit" name="update" id="update">Update Schedule</button>
-        </form>
-    </div>
+    <form action="" method="post" enctype="multipart/form-data">
+        <input type="file" name="schedule_file" accept=".pdf">
+        <input type="hidden" name="schedule_type" value="update2">
+        <button type="submit" name="update" id="update">Update Schedule</button>
+    </form>
+</div>
 </body>
 </html>
