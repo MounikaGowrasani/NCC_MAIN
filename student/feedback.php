801cfa3597995184414fa6f347a82de449eaef97
Hemasri Yakkati <hemasri.yakkati1816@gmail.com>
2023-10-05 08:38:12 +0530

    hema

--- student/feedback.php
+++ student/feedback.php
@@ -6,7 +6,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     session_start();
     // Check if it's the 1st day of the month
     $username = $_SESSION['uname'];
-    if (date('j') == 1 or date('j')== 5 ) {
+    if (date('j') == 1 or date('j')== 15 ) {
         
         $feedback = $_POST["feedback"];
         $rating = $_POST["rating"];
